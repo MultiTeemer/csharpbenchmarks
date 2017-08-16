9f74ce8144ec988806312aedd75ef464d8313378
Artem Khoschenko <multiteemer@gmail.com>
2017-08-16 17:57:30 +1000

    Add call vs callvirt benchmark

---
+++ Benchmarks/Benchmarks/CallVsCallvirt.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Order;
+
+namespace Benchmarks.Benchmarks
+{
+    [OrderProvider(SummaryOrderPolicy.FastestToSlowest)]
+    public class CallVsCallvirt
+    {
+        private class Dumb
+        {
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public static int StaticMethod()
+            {
+                return 1;
+            }
+
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public int NonVirtualMethod()
+            {
+                return 1;
+            }
+
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public virtual int VirtualMethod()
+            {
+                return 1;
+            }
+        }
+        
+        [Benchmark(Baseline = true)]
+        public int StaticCall()
+        {
+            return Dumb.StaticMethod();
+        }
+
+        [Benchmark]
+        public int CallNonVirtualMethod()
+        {
+            return new Dumb().NonVirtualMethod();
+        }
+
+        [Benchmark]
+        public int CallVirtNonVirtualMethod()
+        {
+            var dumb = new Dumb();
+            return dumb.NonVirtualMethod();
+        }
+
+        [Benchmark]
+        public int CallVirtVirtualMethod()
+        {
+            var dumb = new Dumb();
+            return dumb.VirtualMethod();
+        }
+    }
+}
---
+++ Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Running;
+using Benchmarks.Benchmarks;
+
+namespace Benchmarks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchmarkRunner.Run<CallVsCallvirt>();
+		}
+	}
+}
